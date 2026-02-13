bdf277436b7035842babad8d20997a1bbead4e3e
afrandormd <afrandormd@gmail.com>
2026-02-14 03:43:30 +0700

    feat: converting member transactions page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ import '../styles/sign-up-success.css'
 import '../styles/404-not-found.css'
 import '../styles/overview.css'
 import '../styles/sidebar.css'
+import '../styles/transactions.css'
+import '../styles/transactions-detail.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
